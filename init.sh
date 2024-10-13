dbf06a9eb3780bf3fbddfc3b69cae467f5865fa2
songliyu <songliyu@tencent.com>
2024-10-13 20:11:12 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+
+docker pull postgres
 docker run \
-    -id \
-    --name=postgresql \
+    -d \
+    --name mypostgres \
     -v ~/postgresql/data:/var/lib/postgresql/data \
     -p 5432:5432 \
     -e POSTGRES_USER=myadmin \
     -e POSTGRES_PASSWORD=Tcdn@2007 \
     postgres
+
+docker pull redis
+docker run \
+    -d \
+    --name myredis \
+    -p 6379:6379 \
+    redis
